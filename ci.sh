8517c6d57a3c33c8122468a97da21fbfd34417fc
Nikita Bakaev <ya@nbakaev.ru>
2017-03-27 13:18:47 +0300

    add ci script

---
+++ ci.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# require maven, xmlstarlet
+# linux: apt-get install xmlstarlet
+# windows: https://freefr.dl.sourceforge.net/project/xmlstar/xmlstarlet/1.6.1/xmlstarlet-1.6.1-win32.zip
+
+set -e
+
+# set parent version of each module to the latest base module
+find . -name 'pom.xml' | while read line; do
+    xmlstarlet ed --inplace -N p=http://maven.apache.org/POM/4.0.0 -u "/p:project/p:parent/p:version" -v ${PROJECT_VERSION} ${line}
+    echo "Processing module '$line'"
+done
